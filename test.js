d726ff93945aa117ce491870796b6fa808f0e63d
Pradeep <pradeepvenky12@gmail.com>
2021-11-30 17:12:54 -0500

    compute the power of n

--- test.js
+++ test.js
@@ -1,4 +1,20 @@
-let counter = 0
-typeof counter 
-let b = ++ counter
-console.log({b,b})
+// let counter = 0
+// typeof counter 
+// let b = ++ counter
+// console.log({b,b})
+
+const computePower = function(x,y){
+
+    if(x && y){
+        let start = 0
+        let value = 1
+         while(start<y) {
+               value = value*x
+               start ++  
+           }
+    return value 
+          
+    }
+    }
+   const result =  computePower(0,3)
+   console.log({result,result})
